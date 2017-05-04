db6df41c849cc7f525aeef8ecb0341a0cec9db9f
Tobias Lofton <Tobias Lofton>
2017-05-04 17:00:45 -0500

    Added Slick Slider and dependencies to project including Jquery

---
+++ js/showcase.js
@@ -0,0 +1,20 @@
+function main() {
+
+  $('.sliderImages').slick({
+    /*slidesToShow: 1,*/
+    /*slidesToScroll: 1,*/
+     autoplay: true,
+     arrows: false,
+     infinite: true,
+     speed: 500,
+     lazyLoad: 'ondemand',
+     lazyLoadBuffer: 0,
+     fade: true,
+     cssEase: 'linear'
+  });
+
+
+
+}
+
+$(document).ready(main());
