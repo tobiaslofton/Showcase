b867e129f4811f849f5eb05aaa16ff65ececfeed
Toby Koala <tobykoala@Tobys-MacBook-Pro.local>
2017-06-12 12:22:35 -0500

    Adding gulp to project slowly

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sass = require('gulp-sass');
 
 gulp.task('copy', function(){
   gulp.src('showcase.html').pipe(gulp.dest('assets'));
+});
+
+gulp.task('log', function(){
+  gutil.log('My Log Task'); 
 });
